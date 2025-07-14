8607d9da54ebb0403c6756e4b31e0618e87a30ef
daironromano <daironromano@gmail.com>
2025-07-13 23:54:19 -0300

    Assistente Inteligente

---
+++ AssistenteInteligente/main.py
@@ -0,0 +1,31 @@
+import requests
+from decouple import config
+from pprint import pprint
+
+OPENAI_API_KEY = config('OPENAI_API_KEY')
+
+api_url = 'https://api.openai.com/v1'
+
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {OPENAI_API_KEY}'
+}
+
+data = {
+    "model": "gpt-4.1-mini",
+    "messages": [
+        {
+            "role": "user",
+            "content": "Me conte uma piada sobre IA"
+        }
+    ]
+}
+try:
+
+    resp = requests.post(f'{api_url}/chat/completions', headers=headers, json=data)
+    body = resp.json()
+
+    pprint(body, indent=4)
+
+except Exception as e:
+    raise print(e)
